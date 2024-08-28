03b9fde621a2e20f403021f8497849a8064e9992
valenpelaez99 <valenpelaez99mail.com>
2024-08-27 22:06:07 -0500

    Primer commit. empezando a trabajar en odontologo

---
+++ src/main/java/com/backend/clinica/controller/PacienteController.java
@@ -0,0 +1,62 @@
+package com.backend.clinica.controller;
+
+
+import com.backend.clinica.dto.entrada.PacienteEntradaDto;
+import com.backend.clinica.dto.salida.PacienteSalidaDto;
+
+import com.backend.clinica.service.IPacienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    private IPacienteService pacienteService;
+
+    public PacienteController(IPacienteService pacienteService) {
+        this.pacienteService = pacienteService;
+    }
+
+
+    //JSON -> @RequestBody -> DTO Entrada -> controller --> service -> mapper = entidad --> repository --> BDD
+    //--> entidad --> repository --> service -> mapper -> DTO Salida --> controller -> @ResponseBody -> JSON --> cliente
+
+    //POST
+    @PostMapping("/registrar")
+    public ResponseEntity<PacienteSalidaDto> registrarPaciente(@RequestBody @Valid PacienteEntradaDto pacienteEntradaDto){
+        PacienteSalidaDto pacienteSalidaDto = pacienteService.registrarPaciente(pacienteEntradaDto);
+        return new ResponseEntity<>(pacienteSalidaDto, HttpStatus.CREATED);
+    }
+
+    //GET
+    @GetMapping("/listar")
+    public ResponseEntity<List<PacienteSalidaDto>> listarPacientes(){
+        return new ResponseEntity<>(pacienteService.listarPacientes(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")//localhost:8080/pacientes/x
+    public ResponseEntity<PacienteSalidaDto> buscarPacientePorId(@PathVariable Long id){
+        return new ResponseEntity<>(pacienteService.buscarPacientePorId(id), HttpStatus.OK);
+    }
+
+    //PUT
+    @PutMapping("/actualizar/{id}")
+    public ResponseEntity<PacienteSalidaDto> actualizarPaciente(@RequestBody @Valid PacienteEntradaDto paciente, @PathVariable Long id){
+        return new ResponseEntity<>(pacienteService.actualizarPaciente(paciente, id), HttpStatus.OK);
+    }
+
+    //DELETE
+    @DeleteMapping("/eliminar")//localhost:8080/pacientes/eliminar?id=x
+    public ResponseEntity<String> eliminarPaciente(@RequestParam Long id){
+        pacienteService.eliminarPaciente(id);
+        return new ResponseEntity<>("Paciente eliminado correctamente", HttpStatus.NO_CONTENT);
+    }
+
+
+
+}
---
+++ src/main/java/com/backend/clinica/service/IPacienteService.java
@@ -0,0 +1,20 @@
+package com.backend.clinica.service;
+
+import com.backend.clinica.dto.entrada.PacienteEntradaDto;
+import com.backend.clinica.dto.salida.PacienteSalidaDto;
+import com.backend.clinica.entity.Paciente;
+
+import java.util.List;
+
+public interface IPacienteService {
+
+    PacienteSalidaDto registrarPaciente(PacienteEntradaDto paciente);
+    PacienteSalidaDto buscarPacientePorId(Long id);
+
+    List<PacienteSalidaDto> listarPacientes();
+
+    void eliminarPaciente(Long id);
+    PacienteSalidaDto actualizarPaciente(PacienteEntradaDto pacienteEntradaDto, Long id);
+
+
+}
